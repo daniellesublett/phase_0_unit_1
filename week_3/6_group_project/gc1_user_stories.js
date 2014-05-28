add05f6c7e716c74a32f09baf11d809ea6d035b4
Danielle Sublett <danielle.sublett@gmail.com>
2014-05-28 09:26:57 -0700

    3/6 Group Project Release 1

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,20 @@
+// My role in the group is: Person 1, Tests to User Stories
+
+// Here is my part of the challenge:
+
+//There are 2 groups consisting of diferent numbers. 
+//One group has an even amount of numbers, the other group has an odd amount of numbers.
+//
+// I need to get the sum of all the numbers in the group 
+// that has an ODD amount of numbers.
+// 
+// Then I need to get the sum of all the numbers in the group 
+// that has an EVEN amount of numbers. 
+//
+// Next I need the mean (average) of all the numbers in the 
+// group that has an ODD amount of numbers. I'll also need the
+// mean of the group that has an EVEN amount of numbers.
+// 
+// Lastly I need to find the median of the EVEN group, 
+// and very lastly I need the median of the ODD group of numbers.
+// 
