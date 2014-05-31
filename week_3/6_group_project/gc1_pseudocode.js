c7ce9e1627cee92cd1e081a184562169306cf0ee
Danielle Sublett <danielle.sublett@gmail.com>
2014-05-31 11:51:07 -0700

    week3 group project step 2

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,48 @@
+// My role in the group is: Person 1 & 2, Tests to User Stories, and user stories into pseudocode
+
+// Here is my part of the challenge:
+
+//USER STORY
+//There are 2 groups consisting of diferent numbers. 
+//One group has an even amount of numbers, the other group has an odd amount of numbers.
+//
+////////////PSEUDOCODE
+//////// We will be using two variables, evenLengthArray and oddLengthArray
+// 
+//USER STORY
+// I need to get the sum of all the numbers in the group 
+// that has an ODD amount of numbers.
+//
+////////////PSEUDOCODE
+/////// We need a function, sum, where we do a "for" loop through the numbers in oddLengthArray and total them up, 
+/////// then return the total.
+// 
+//USER STORY
+// Then I need to get the sum of all the numbers in the group 
+// that has an EVEN amount of numbers. 
+//
+////////////PSEUDOCODE
+////// Using our sum function we created above, we need to do the same for the evenLengthArray. 
+//
+//USER STORY
+// Next I need the mean (average) of all the numbers in the 
+// group that has an ODD amount of numbers. I'll also need the
+// mean of the group that has an EVEN amount of numbers.
+//
+////////////PSEUDOCODE
+////// We need to do a "for" loop through the numbers, iterating 
+////// through every number in oddArray and total them up, then return the total divided by the 
+////// length of the array. Then we need to do the same (iterate, get the sum, divide the sum by the array length)
+////// for the evenArray.
+// 
+//USER STORY
+// Lastly I need to find the median of the EVEN group, 
+// and very lastly I need the median of the ODD group of numbers.
+//
+////////////PSEUDOCODE
+////// We need to sort oddLengthArray and EvenLengthArray and return the numbers at the CENTER
+////// of the array for oddLengthArray. This means that we will need to pull the CENTER 2 from EvenLengthArray and average them.
+// 
+
+
+
